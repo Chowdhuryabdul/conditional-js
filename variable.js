d90cdeb3bd3c18ea8c3371a43d1ea693db065f8b
Abdul Quaiyum <abdulquaiyum@Mac.home>
2026-01-07 13:19:31 +0100

    first commit

---
+++ variable.js
@@ -0,0 +1,8 @@
+/* variable is a tyeps of container */
+/* there are three types of variables */
+/* Number type variable */
+var sunglassPrice = 30;
+/* String type variable */
+var subject = "EEE"
+/* Boolean type variable */
+var passed = true;
